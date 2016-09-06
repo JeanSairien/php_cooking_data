69c453c7e2dacc1bc7a8866692ba6fb1a74560d0
Redfish <lesonduko@gmail.com>
2016-09-06 15:42:34 +0200

    fin de resolution du cooking data

--- film.php
+++ film.php
@@ -25,22 +25,73 @@ $top = $brut["feed"]["entry"]; # liste de films
 <h1>Le top10 des films</h1>
 
 	<pre>
+	<p>afficher le top 10</p>
+
 
 	<?php 
-		foreach ($top as $key => $value) {
+		 echo "<ol>";
+		for($i=0;$i < 10;$i++) {
 			
-		echo $value['im:name']['label'];
-		
+
+				echo "<li>".$top[$i]['im:name']['label']."</li>";
+			}
+				echo "</ol>";
+	?>
+	<p>rechercher la position de gravity</p>
+	<?php 
+	echo "<ol>";
+		for($i = 0; $i < count($top); $i++){
+			if($top[$i-1]['im:name']['label'] == "Gravity")
+				echo "<li>". $i ."</li>" ;		
 		}
-			
-		
-		
+			echo "</ol>" ;
+
+	?>
+	<p>rechercher le realisateur de lego movies</p>
 	
-	print_r($top)  ;
+	<?php 
+	echo "<ol>";
+		for($i = 0; $i < count($top); $i++){
+			if($top[$i]['im:name']['label'] == "The LEGO Movie")
+				echo "<li>". $top[$i]['im:artist']['label'] ."</li>" ;		
+		}
+			echo "</ol></br>" ;
+	?>
+		
+
+	<p>Les films sortit avant 2000</p>
+	<?php
+		echo "<ol>";
+		for($i = 0; $i < count($top); $i++){
+			if($top[$i]['im:releaseDate']['label'] <= "2000"){
+				echo "<li>". $top[$i]['im:releaseDate']['label'] ."</li>" ;		
+			}
+		}
+			echo "</ol>" ;
+	?>
+	
+	<p>Le film le plus vieux  </p>
+	<?php
+		$old =array();
+		echo "<ol>";
+		for($i = 0; $i < 100; $i++){
+			if($top[$i]['im:releaseDate']['label'] <= "2000"){
+				echo "<li>". $top[$i]['im:releaseDate']['label'] ." est l'année du film le plus vieux"."</li>" ;	
+				array_push($old, $top[$i]['im:releaseDate']['label']);
+				
+					
+			}
+		}
+			echo "</ol>" ;
+			echo $old ;
 
 	?>
 
+
+
 	</pre>
+	 
+		
 
 </body>
-</html>
+</html> 
